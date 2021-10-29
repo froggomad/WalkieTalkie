f2172d1c01e2f70c9919f87f0598b8191b0f1c31
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2021-10-29 15:42:13 -0700

    added `ClearButton` modifier for TextFields

---
+++ WalkieTalkie/Features/Search/View/ClearButtonViewModifier.swift
@@ -0,0 +1,27 @@
+//
+//  ClearButtonViewModifier.swift
+//  WalkieTalkie
+//
+//  Created by Kenneth Dubroff on 10/29/21.
+//
+
+import SwiftUI
+
+public struct ClearButton: ViewModifier {
+    @Binding var text: String
+
+    public init(text: Binding<String>) {
+        self._text = text
+    }
+
+    public func body(content: Content) -> some View {
+        HStack {
+            content
+            Spacer()
+            Image(systemName: "multiply.circle.fill")
+                .foregroundColor(.secondary)
+                .opacity(text == "" ? 0 : 1)
+                .onTapGesture { self.text = "" }
+        }
+    }
+}
