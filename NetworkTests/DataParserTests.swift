5ad36787bbd784be0df60886ccec24c08bd0f284
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2021-10-30 16:22:31 -0700

    implemented test to ensure invalid json doesn't decode

--- NetworkTests/DataParserTests.swift
+++ NetworkTests/DataParserTests.swift
@@ -11,11 +11,24 @@ import XCTest
 class DataParserTests: XCTestCase {
 
     func testDataParser_canDecodeMockRecordings() {
-        let parser = DataParser()
         let data =  AudioRecording.mockData
-        let decodedData: [AudioRecording]? = try? parser.parse(data: data)
+        let decodedData: [AudioRecording]? = try? sut.parse(data: data)
         XCTAssertNotNil(decodedData)
         XCTAssertEqual(decodedData?.count, 10)
+    }
+    
+    func testDataParser_failsDecodingInvalidJSON() {
+        let data =
+        """
+        recordings: [{ "id":1,"username_from":"farbrother0","timestamp":"1606314693","recording":"/recordings/example_transmission.mp3","username_to":"kyle_ski"
+        }]
+        """.data(using: .utf8)!
+        let decodedData: [AudioRecording]? = try? sut.parse(data: data)
+        XCTAssertNil(decodedData)
+    }
+    
+    var sut: DataParser {
+        DataParser()
     }
 
 }
