1c353a60b91b61745f078ff429be626a1c0b7ecd
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2021-10-31 09:57:11 -0700

    using `HistoryView`'s default initializer

--- WalkieTalkie/App/WalkieTalkieApp.swift
+++ WalkieTalkie/App/WalkieTalkieApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct WalkieTalkieApp: App {
     var body: some Scene {
         WindowGroup {
-            HistoryView(viewModel: AudioRecordingViewModel(audioService: AudioService()))
+            HistoryView()
         }
     }
 }
