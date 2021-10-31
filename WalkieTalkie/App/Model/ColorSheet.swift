62faf608e17f2612c23c35fae0f5ad552b4fb881
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2021-10-31 09:51:08 -0700

    added style sheet and method to change navigation title color

---
+++ WalkieTalkie/App/Model/ColorSheet.swift
@@ -0,0 +1,15 @@
+//
+//  ColorSheet.swift
+//  WalkieTalkie
+//
+//  Created by Kenneth Dubroff on 10/31/21.
+//
+
+import SwiftUI
+
+struct ColorSheet {
+    static let primaryColor = Color.blue
+    static let actionColor = Color.orange
+    static let darkText = Color.black
+    static let lightText = Color.white
+}
---
+++ WalkieTalkie/App/Model/View+navigationBarTitleTextColor.swift
@@ -0,0 +1,25 @@
+//
+//  View+navigation.swift
+//  WalkieTalkie
+//
+//  Created by Kenneth Dubroff on 10/31/21.
+//
+
+import SwiftUI
+
+extension View {
+    /// Sets the text color for a navigation bar title.
+    /// - Parameter color: Color the title should be
+    ///
+    /// Supports both regular and large titles.
+    @available(iOS 14, *)
+    func navigationBarTitleTextColor(_ color: Color) -> some View {
+        let uiColor = UIColor(color)
+    
+        // Set appearance for both normal and large sizes.
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: uiColor ]
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: uiColor ]
+    
+        return self
+    }
+}
