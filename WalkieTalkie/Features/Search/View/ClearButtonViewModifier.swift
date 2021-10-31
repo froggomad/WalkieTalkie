7f36ad712f1165283f8f2e54515b86cbae3f9df1
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2021-10-31 09:56:23 -0700

    Moved cancel button closer to search bar

--- WalkieTalkie/Features/Search/View/ClearButtonViewModifier.swift
+++ WalkieTalkie/Features/Search/View/ClearButtonViewModifier.swift
@@ -9,19 +9,22 @@ import SwiftUI
 
 public struct ClearButton: ViewModifier {
     @Binding var text: String
-
+    
     public init(text: Binding<String>) {
         self._text = text
     }
-
+    
     public func body(content: Content) -> some View {
         HStack {
-            content
-            Spacer()
-            Image(systemName: "multiply.circle.fill")
-                .foregroundColor(.secondary)
-                .opacity(text == "" ? 0 : 1)
-                .onTapGesture { self.text = "" }
+            ZStack {
+                content
+                Image(systemName: "multiply.circle.fill")
+                    .foregroundColor(.secondary)
+                    .opacity(text == "" ? 0 : 1)
+                    .onTapGesture { self.text = "" }
+                    .foregroundColor(ColorSheet.lightText)
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+            }
         }
     }
 }
