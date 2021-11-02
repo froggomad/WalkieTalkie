fc995c50a44399308f491eec8a434c030c65c4c7
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2021-11-01 17:31:51 -0700

    renamed `RefreshView` -> `RefreshControl`

---
+++ WalkieTalkie/Features/PullToRefresh/RefreshControl.swift
@@ -0,0 +1,22 @@
+//
+//  RefreshControl.swift
+//  WalkieTalkie
+//
+//  Created by Kenneth Dubroff on 11/1/21.
+//
+import SwiftUI
+/// This class provides pull-to-refresh functionality. However, a loading indicator must be provided outside of the class
+struct RefreshControl: View {
+    var coordinateSpace: CoordinateSpace
+    var onRefresh: () -> Void
+    var body: some View {
+        GeometryReader { geo in
+            if (geo.frame(in: coordinateSpace).midY > 250) {
+                Spacer()
+                    .onAppear {
+                        onRefresh() //call refresh once if pulled more than 50px0
+                    }
+            }
+        }
+    }
+}
