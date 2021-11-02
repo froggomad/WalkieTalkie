f5d78939067163c3eac2e5fc0cb101d0143ae563
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2021-11-01 20:49:14 -0700

    added `HistoryProgressView` to keep ProgressView styling consistent

---
+++ WalkieTalkie/App/Views/HistoryProgressView.swift
@@ -0,0 +1,29 @@
+//
+//  HistoryProgressView.swift
+//  WalkieTalkie
+//
+//  Created by Kenneth Dubroff on 11/1/21.
+//
+
+import SwiftUI
+
+struct HistoryProgressView: View {
+    var color: Color = ColorSheet.lightText
+    
+    var body: some View {
+        ProgressView("loading...")
+            .padding(.top, 20)
+            .foregroundColor(color)
+            .progressViewStyle(CircularProgressViewStyle(tint: color))
+    }
+}
+
+struct HistoryProgressView_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            Color.black
+            HistoryProgressView()
+        }
+        HistoryProgressView(color: .black)
+    }
+}
