23ff2d2114ff6057ae93e6282c9aba46f992cca2
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2021-11-09 21:44:37 -0800

    Created `SaveButton`

---
+++ WalkieTalkie/Features/Persistence/SaveButton.swift
@@ -0,0 +1,48 @@
+//
+//  SaveButton.swift
+//  WalkieTalkie
+//
+//  Created by Kenneth Dubroff on 11/9/21.
+//
+
+import SwiftUI
+
+
+struct SaveButton: View {
+    @State var persistenceService: AudioRecordingPersistenceService
+    @State var isRecordingSaved: Bool = false
+
+    var recording: AudioRecording
+    var recordingType: RecordingType
+
+    var body: some View {
+        VStack {
+            if !isRecordingSaved {
+                Button("Save Recording") {
+                    persistenceService.save(recording, of: recordingType) { result in
+                        switch result {
+                        case let .success(result):
+                            isRecordingSaved = result
+                        case let .failure(error):
+                            print(error)
+                        }
+                    }
+                }
+            } else {
+                Button("Delete Recording") {
+                    // TODO: Delete recording
+                    isRecordingSaved = false
+                }
+            }
+        }
+        .onAppear {
+            self.isRecordingSaved = persistenceService.isRecordingSaved(recording, of: recordingType)
+        }
+    }
+}
+
+struct SaveButton_Previews: PreviewProvider {
+    static var previews: some View {
+        SaveButton(persistenceService: AudioRecordingPersistenceService(user: User(username: "admin", userType: .admin)), recording: AudioRecording.previewRecording, recordingType: .outgoing)
+    }
+}
